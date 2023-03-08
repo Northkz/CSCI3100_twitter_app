39c656da97dbc95ad5da090f527e1aaeb4e2341e
AdkhamovaMunissa <1155147488@link.cuhk.edu.hk>
2023-03-08 22:39:37 +0800

    Update Step1.js

--- code/Register/pages/Step1.js
+++ code/Register/pages/Step1.js
@@ -12,7 +12,7 @@ export default function Step1() {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
         </Head>  
         <Script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" /> 
-        <div className='container-lg'>
+        <div className='container'>
          <nav className='row row-cols-2 bg-primary'>
             <div className='col-1'>
               <Link type="button" className="btn-close" href='/'></Link>
@@ -22,36 +22,34 @@ export default function Step1() {
               <p>Step 1 of 2</p>
             </div>
          </nav>
-         <div className='col gy-2'>
+         <div className='col'>
           <form noValidate>
-            <h1 className='header'>Create your account</h1>
+            <h1 className='header mt-3 mb-2'>Create your account</h1>
             <div className='form-floating'>
-              <input type='text' id='name' className='form-control' placeholder='Name' required></input>
-              <label for='text'>Name</label>
+              <input type='text' id='name' className='form-control mb-2' placeholder='Name' required></input>
+              <label>Name</label>
               <div className='valid-feedback'>Valid</div>
               <div className='invalid-feedback'>Invalid</div>
             </div>
             
             <div className='form-floating'>
               <input type='email' className='form-control' placeholder='Email' required></input>
-              <label for='email'>Email</label>
+              <label>Email</label>
               <div className='valid-feedback'>Valid</div>
               <div className='invalid-feedback'>Invalid</div>
             </div>
 
-            <div className='col'>
+            <div className='col my-3'>
 
-              <h3>Date of birth</h3>
-              <p>Confirm your age, please. This information will not be shown publicly</p>
+              <h5 className='mb-1'>Date of birth</h5>
+              <p className='mb-2'>Confirm your age, please. This information will not be shown publicly</p>
               <div className='row row-cols-3 g-1'>
                 <input className='col' type='month' placeholder='Month' required></input>
-                {/* <input className='col' type='day' placeholder='Day'></input>
-                <input className='col' type='year' placeholder='Year'></input> */}
               </div>
 
             </div>
 
-            <button>Submit</button>
+            <button className='btn btn-dark'>Submit</button>
           </form>
          </div>
       </div>
