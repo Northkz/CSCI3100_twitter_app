df1fea2a4eea858c17a46171b967f8e0604438d8
Northkz <nursultankambar339@gmail.com>
2023-04-03 04:39:57 +0800

    finished back for auth

---
+++ backend/tweets/models.py
@@ -0,0 +1,5 @@
+import random
+from django.conf import settings
+from django.db import models
+from django.db.models import Q
+
---
+++ backend/tweets/views.py
@@ -0,0 +1,5 @@
+import random
+from django.conf import settings
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render, redirect
+from django.utils.http import url_has_allowed_host_and_scheme
