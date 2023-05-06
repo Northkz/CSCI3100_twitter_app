44eb0141b481ed53da219648794a99df86b47100
Munkhbilegb <munkhbileg@link.cuhk.edu.hk>
2023-05-06 23:51:14 +0800

    delete old version

--- back/notifications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-
-urlpatterns = [
-    path('notification_list/', views.NotificationView, name="notification-list"),
-    path('notification_seen_delete/',
-         views.NotificationSeen.as_view(), name="notification-seen"),
-]
